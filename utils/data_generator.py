3b21e3e97f12c916de02cd620f139fd2733d017c
Alex Aramyan <alexaramyan@Alexs-MacBook-Pro.local>
2022-05-27 00:10:36 +0400

    data generation for siamese network feature added

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -14,35 +14,76 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
         self.dim = properties["target_size"]
         
         self._list_folders = self.__get_folders_list(directory)
-        self.pair_combinations = self.__get_all_combinations()
+        # self.pair_combinations = self.__get_all_combinations()
 
-        print(self.pair_combinations[:10])
+        print(self._list_folders[:10])
         
     def __len__(self):
         return int(len(self._list_folders) / self.batch_size)
     
     def __getitem__(self, index):
-        is_same = np.random.choice([False, True], p=[0.8, 0.2])
         
         x_batch = np.ones((self.batch_size, 2, self.dim[0], self.dim[1], 1))
         y_batch = np.ones((self.batch_size, 1))
-        
+
+        # img1_path = np.random.choice(self._list_folders)
+        # print(np.random.choice(glob.glob(img1_path + "/*.*")))
+        # print(os.path.join(img1_path, np.random.choice(np.random.choice(glob.glob(img1_path + "/*.*")))))
+
+        print("batch size:", self.batch_size)
+
         for i in range(self.batch_size):
-            img1 = self.__load_img(self.pair_combinations[i][0])
-            img2 = self.__load_img(self.pair_combinations[i][1])
-    
+            is_same = np.random.choice([0, 1], p=[0.8, 0.2])
+
+            img1_path = np.random.choice(self._list_folders)
+            img1 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img1_path) + "/*.*")))
+            
+            # print("is_same:", is_same)
+
+            if is_same == 1:
+                img2 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img1_path) + "/*.*")))
+                # y_batch[i] = 1
+
+                # print("true y_batch:", y_batch, "i_true ", i)
+            else:
+                img2_path = np.random.choice(self._list_folders)
+                while img2_path == img1_path:
+                    img2_path = np.random.choice(self._list_folders)
+                img2 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img2_path) + "/*.*")))
+
+                y_batch[i] = 0
+                
+                # print("false batch:", y_batch, "i_false: ", i)
+
             x_batch[i][0] = img1
             x_batch[i][1] = img2
 
-            if self.pair_combinations[i][0][len(self.directory) + 1 : len(self.directory) + 4] == self.pair_combinations[i][1][len(self.directory) + 1 : len(self.directory) + 4]:
-                y_batch[i] = 1.0
-            else:
-                y_batch[i] = 0.0
-                        
+
         return x_batch, y_batch
+
+    # def __getitem__(self, index):
+    #     is_same = np.random.choice([False, True], p=[0.8, 0.2])
+        
+    #     x_batch = np.ones((self.batch_size, 2, self.dim[0], self.dim[1], 1))
+    #     y_batch = np.ones((self.batch_size, 1))
+        
+    #     for i in range(self.batch_size):
+    #         img1 = self.__load_img(self.pair_combinations[i][0])
+    #         img2 = self.__load_img(self.pair_combinations[i][1])
+    
+    #         x_batch[i][0] = img1
+    #         x_batch[i][1] = img2
+
+    #         if self.pair_combinations[i][0][len(self.directory) + 1 : len(self.directory) + 4] == self.pair_combinations[i][1][len(self.directory) + 1 : len(self.directory) + 4]:
+    #             y_batch[i] = 1.0
+    #         else:
+    #             y_batch[i] = 0.0
+                        
+    #     return x_batch, y_batch
           
     def on_epoch_end(self):
-        np.random.shuffle(self.pair_combinations)
+        np.random.shuffle(self._list_folders)
+        # np.random.shuffle(self.pair_combinations)
         
     def __load_img(self, path):
         img = cv2.imread(path, 0)
@@ -55,6 +96,9 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
         return sorted(list(itertools.combinations(glob.glob(os.path.join(self.directory, "*", "*")), 2)))
     
     def __get_folders_list(self, path):
-        _list = glob.glob(os.path.join(self.directory, "*"))
+        _list = glob.glob(os.path.join(path, "*"))
+
+        for i in range(len(_list)):
+            _list[i] = _list[i][len(path) + 1:]
         
         return _list
