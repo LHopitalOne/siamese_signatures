2f67340ab0afa310108cdb8ad8eb71ac37d47681
Alex Aramyan <alexaramyan@Alexs-MacBook-Pro.local>
2022-05-29 00:02:57 +0400

    generator configured for siamese model

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -22,13 +22,8 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
         return int(len(self._list_folders) / self.batch_size)
     
     def __getitem__(self, index):
-        
         x_batch = np.ones((self.batch_size, 2, self.dim[0], self.dim[1], 1))
         y_batch = np.ones((self.batch_size, 1))
-
-        # img1_path = np.random.choice(self._list_folders)
-        # print(np.random.choice(glob.glob(img1_path + "/*.*")))
-        # print(os.path.join(img1_path, np.random.choice(np.random.choice(glob.glob(img1_path + "/*.*")))))
 
         print("batch size:", self.batch_size)
 
@@ -42,7 +37,6 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
 
             if is_same == 1:
                 img2 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img1_path) + "/*.*")))
-                # y_batch[i] = 1
 
                 # print("true y_batch:", y_batch, "i_true ", i)
             else:
@@ -58,37 +52,18 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
             x_batch[i][0] = img1
             x_batch[i][1] = img2
 
-
         return x_batch, y_batch
 
-    # def __getitem__(self, index):
-    #     is_same = np.random.choice([False, True], p=[0.8, 0.2])
-        
-    #     x_batch = np.ones((self.batch_size, 2, self.dim[0], self.dim[1], 1))
-    #     y_batch = np.ones((self.batch_size, 1))
-        
-    #     for i in range(self.batch_size):
-    #         img1 = self.__load_img(self.pair_combinations[i][0])
-    #         img2 = self.__load_img(self.pair_combinations[i][1])
-    
-    #         x_batch[i][0] = img1
-    #         x_batch[i][1] = img2
-
-    #         if self.pair_combinations[i][0][len(self.directory) + 1 : len(self.directory) + 4] == self.pair_combinations[i][1][len(self.directory) + 1 : len(self.directory) + 4]:
-    #             y_batch[i] = 1.0
-    #         else:
-    #             y_batch[i] = 0.0
-                        
-    #     return x_batch, y_batch
-          
     def on_epoch_end(self):
         np.random.shuffle(self._list_folders)
-        # np.random.shuffle(self.pair_combinations)
         
     def __load_img(self, path):
         img = cv2.imread(path, 0)
         img = cv2.resize(img, self.dim)
+        img = np.expand_dims(img, axis=0)
         img = np.expand_dims(img, axis=-1).astype(np.float32) / 255.0
+
+        # print("shape:", img.shape)
         
         return img
     
