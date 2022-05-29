ff9e114b1c9fb79f45902c94e48bde838b05b299
Alex Aramyan <alexaramyan@Alexs-MacBook-Pro.local>
2022-05-29 21:32:42 +0400

    bug fixed

--- utils/data_generator.py
+++ utils/data_generator.py
@@ -25,12 +25,14 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
         x_batch = np.ones((self.batch_size, 2, self.dim[0], self.dim[1], 1))
         y_batch = np.ones((self.batch_size, 1))
 
-        print("batch size:", self.batch_size)
+        # print("batch size:", self.batch_size)
+
+        _list_folders = self._list_folders[index * self.batch_size : (index + 1) * self.batch_size]
 
         for i in range(self.batch_size):
             is_same = np.random.choice([0, 1], p=[0.8, 0.2])
 
-            img1_path = np.random.choice(self._list_folders)
+            img1_path = np.random.choice(_list_folders)
             img1 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img1_path) + "/*.*")))
             
             # print("is_same:", is_same)
@@ -40,9 +42,9 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
 
                 # print("true y_batch:", y_batch, "i_true ", i)
             else:
-                img2_path = np.random.choice(self._list_folders)
+                img2_path = np.random.choice(_list_folders)
                 while img2_path == img1_path:
-                    img2_path = np.random.choice(self._list_folders)
+                    img2_path = np.random.choice(_list_folders)
                 img2 = self.__load_img(np.random.choice(glob.glob(os.path.join(self.directory, img2_path) + "/*.*")))
 
                 y_batch[i] = 0
@@ -60,7 +62,7 @@ class SiameseDataGenerator(tf.keras.utils.Sequence):
     def __load_img(self, path):
         img = cv2.imread(path, 0)
         img = cv2.resize(img, self.dim)
-        img = np.expand_dims(img, axis=0)
+        # img = np.expand_dims(img, axis=0)
         img = np.expand_dims(img, axis=-1).astype(np.float32) / 255.0
 
         # print("shape:", img.shape)
